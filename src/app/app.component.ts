9ba47ba847a4224fca21d01808e1a96e090a3ec0
gstanescu <georgestanescu90@gmail.com>
2016-11-21 10:29:31 +0200

    two way data binding - multiple ways of dealing with it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,13 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   templateUrl: `
-    <app-courses></app-courses>
-    <div (click)="onDivClick()">
-      <button class="btn btn-primary" on-click='onClick($event)'>Click one</button>
-      <button class="btn btn-primary" (click)='onClick($event)'>Click two</button>
-    </div> 
+    <input type="text" [value]="content" (input)="content = $event.target.value">
+    <input type="text" [(ngModel)]="content" >
+    <input type="text" bindon-ngModel="content" >
+    <input type="button" value="Clear" (click)="content='' ">
+    <label>Preview: {{ content }}</label>
   `
 })
 export class AppComponent {
-
-  onDivClick() {
-    event.stopPropagation();
-  }
-
-  onClick(event) {
-    event.stopPropagation();
-    console.log("Clicked", event);
-  }
+  content: string = "Some content";
 }
