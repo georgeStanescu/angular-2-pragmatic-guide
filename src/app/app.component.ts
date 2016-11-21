844e1ab593cef261c0dbbd9ea1129715deca7aab
gstanescu <georgestanescu90@gmail.com>
2016-11-21 13:11:32 +0200

    ngSwitch, ngSwitchCase, ngSwitchDefault usages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,15 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   template: `
-    <app-tweet></app-tweet>
+    <ul class="nav nav-pills">
+      <li [class.active]="viewMode =='map'"><a (click)="viewMode='map'">Map View</a></li>
+      <li [class.active]="viewMode =='list'"><a (click)="viewMode='list'">List View</a></li>
+    </ul>
+    <div [ngSwitch]="viewMode">
+      <template [ngSwitchCase]="'map'">Map View Content</template>
+      <template [ngSwitchCase]="'list'">List View Content</template>
+      <template ngSwitchDefault>Default View Content</template>
+    </div>
   `
 })
 export class AppComponent {
