bb2b8de68c11d7a93d1badfcf82d8b1351d0f516
gstanescu <georgestanescu90@gmail.com>
2016-11-22 13:40:37 +0200

    using observables to manipulate DOM elements in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from "@angular/core";
+import {FormGroup, FormBuilder } from "@angular/forms";
+// import { Observable } from "rxjs/Rx";
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: "app-root",
   template: `
-    <app-signup-form></app-signup-form>
+    <form [formGroup]="searchForm">
+      <input type="text" formControlName="searchControl">
+    </form>
   `
 })
 export class AppComponent {
+  searchForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.searchForm = formBuilder.group({
+      searchControl: []
+    });
+
+    let searchControl = this.searchForm.get("searchControl");
+
+    searchControl.valueChanges
+      .debounceTime(1000)
+      .subscribe(x => console.log(x));
+  }
 }
